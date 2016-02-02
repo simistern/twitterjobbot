1f1ec6b8fa7f1bbddf2d04cce69cf2a1651023da
Simon <simon160@gmail.com>
2016-02-02 16:06:01 -0500

    modularized the search keys a bit, figured out a bit more of the json encoding for each tweet

--- bot.js
+++ bot.js
@@ -12,22 +12,29 @@ var TWITTER_ACCESS_TOKEN = '4828358653-ryLfjymyKRlQkbTRhSI39jJWPytQXsylXvSiZj4';
 var TWITTER_ACCESS_TOKEN_SECRET = 'K0goMBSK9Y14SLmGDk05q7ZnHppfXXo6ZVCEbXt26sgF3';
 
 /* Set Twitter search phrase */
-var TWITTER_SEARCH_PHRASE = '#Toronto AND #techjobs';
+//var TWITTER_SEARCH_PHRASE = '#Toronto AND #techjobs';
+
+var TWITTER_SEARCH_PHRASE = StringTheHashtags("#Toronto", "#Jobs", "#tech");
 var incrementer = 0;
 var Twit = require('twit');
 
 var Bot = new Twit({
 	consumer_key: TWITTER_CONSUMER_KEY,
 	consumer_secret: TWITTER_CONSUMER_SECRET,
-	access_token: TWITTER_ACCESS_TOKEN, 
+	access_token: TWITTER_ACCESS_TOKEN,
 	access_token_secret: TWITTER_ACCESS_TOKEN_SECRET
 });
 
 console.log('The bot is running...');
 
+/* Creates a SQL version of the hashtags added (maximum 3) */
+function StringTheHashtags(FirstTag, SecondTag, ThirdTag){
+		return sqlstring = FirstTag + " AND "  + SecondTag + " AND " + ThirdTag;
+}
+
 /* BotInit() : To initiate the bot */
 function BotInit() {
-	
+
 	BotRetweet();
 	//Bot.post('statuses/retweet/:id', { id: '669520341815836672' }, BotInitiated);
 	function BotInitiated (error, data, response) {
@@ -38,43 +45,41 @@ function BotInit() {
   			console.log('Bot initiated : 669520341815836672');
 		}
 	}
-	
 }
 
 /* BotRetweet() : To retweet the matching recent tweet */
 function BotRetweet() {
-
+	var lastidcheck = "";
 	var query = {
 		q: TWITTER_SEARCH_PHRASE,
-		result_type: "recent"
+		result_type: "mixed"
 	}
 
 	Bot.get('search/tweets', query, BotGotLatestTweet);
 
 	function BotGotLatestTweet (error, data, response) {
-	       incrementer++;
+		console.log("text in last status was: " + data.statuses[0].text);
 		if (error) {
 			console.log('Bot could not find latest tweet, : ' + error);
 		}
 		else {
-    		try{
-				var id = {
-					id : data.statuses[incrementer].id_str
-				}
+    	try{
+				var id = { id : data.statuses[0].id_str}
 			}
 			catch(e){
-			console.log('what a time: ' + e);
+				console.log('what a time: ' + e);
 			}
-/*			var name = {
-				name : data.statuses[incrementer].screen_name
-			} */
+
+			function CheckLastTweet(){
+				console.log("Current ID is: " + " and last ID is: " + lastidcheck);
+			}
 
 			Bot.post('statuses/retweet/:id', id, BotRetweeted);
-			
+
 			function BotRetweeted(error, response) {
+				CheckLastTweet();
 				if (error) {
-					console.log('Bot could not retweet, : ' + error);
-					incrementer++;
+					console.log('Bot could not retweet, : ' + error + "the current incrementer value is: " + incrementer + ', and the name of this person is '); //+ JSON.stringify(data.screen_name[1]));
 				}
 				else {
 					//console.log('Bot retweeted : ' + i);
@@ -84,9 +89,9 @@ function BotRetweet() {
 			}
 		}
 	}
-	
+
 	/* Set an interval of 30 minutes (in microsecondes) */
-	setInterval(BotRetweet, 2*60*1000);
+	setInterval(BotRetweet, 1*60*1000);
 }
 
 /* Initiate the Bot */
